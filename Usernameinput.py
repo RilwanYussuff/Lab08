e42a55dc8f387be040127af68d48c57617bc38d3
Rilwan Yussuff <rilwanyussuff@Rilwans-MacBook-Pro.local>
2022-03-21 20:39:05 -0400

    added Usernameinput.py

---
+++ Usernameinput.py
@@ -0,0 +1,7 @@
+
+userName = input("Hello! Please enter your facebook username! ")
+userPassword = input("Hello! Please enter your facebook password! ")
+if(userName .lower() == "rilwan" and  userPassword == "ProgrammingRocks!"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credential, try again!")
